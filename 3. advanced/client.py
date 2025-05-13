ba8a1d2f8de871a3e4bfb8236870eca4cb07882c
HeonjiHwang <hjhwang@langcode.io>
2025-05-13 16:34:22 +0900

    주석 제거거

--- 3. advanced/client.py
+++ 3. advanced/client.py
@@ -9,16 +9,16 @@ print(f"Description: {client.agent_card.description}")
 print(f"Skills: {[skill.name for skill in client.agent_card.skills]}\n")
 
 #Send a greeting
-# request = 'Hello!'
-# print(f"User: {request}")
-# response = client.ask(request)
-# print(f"Agent: {response}\n")
+request = 'Hello!'
+print(f"User: {request}")
+response = client.ask(request)
+print(f"Agent: {response}\n")
 
-# #Send another message
-# request2 = "How's the weather in Paris?"
-# print(f"User: {request2}")
-# response = client.ask(request2)
-# print(f"Agent: {response}\n")
+#Send another message
+request2 = "How's the weather in Paris?"
+print(f"User: {request2}")
+response = client.ask(request2)
+print(f"Agent: {response}\n")
 
 #Send another message
 request3 = "Suggest mild places with museums"
